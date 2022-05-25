d4619b6e00f268de1186cf6470372477e276c5bb
v-zzz <v-zzz.gd@chinatelecom.cn>
2022-05-25 15:34:48 +0800

    init

--- src/main/java/io/futatkotome/mydddplugin/MyAction.java
+++ src/main/java/io/futatkotome/mydddplugin/MyAction.java
@@ -1,4 +1,21 @@
 package io.futatkotome.mydddplugin;
 
-public class MyAction {
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+
+public class MyAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+        Project project = anActionEvent.getData(PlatformDataKeys.PROJECT);
+        PsiFile psiFile = anActionEvent.getData(CommonDataKeys.PSI_FILE);
+        String classPath = psiFile.getVirtualFile().getPath();
+
+        Messages.showMessageDialog(project, "guide-idea-plugin-create-project-by-gradle: " + classPath, "Hi IDEA Plugin", Messages.getInformationIcon());
+    }
 }
