2e842f93e07d0b0ced860a2e979b9752e9db2ee8
v-zzz <v-zzz.gd@chinatelecom.cn>
2022-05-25 16:35:03 +0800

    ui

---
+++ src/main/java/io/futatkotome/mydddplugin/ui/ProjectConfigUI.java
@@ -0,0 +1,31 @@
+package io.futatkotome.mydddplugin.ui;
+
+import javax.swing.*;
+
+public class ProjectConfigUI {
+    private JPanel mainPanel;
+    private JTextField groupIdField;
+    private JTextField artifactIdField;
+    private JTextField versionField;
+    private JTextField packageField;
+
+    public JComponent getComponent() {
+        return mainPanel;
+    }
+
+    public JTextField getGroupIdField() {
+        return groupIdField;
+    }
+
+    public JTextField getArtifactIdField() {
+        return artifactIdField;
+    }
+
+    public JTextField getVersionField() {
+        return versionField;
+    }
+
+    public JTextField getPackageField() {
+        return packageField;
+    }
+}
